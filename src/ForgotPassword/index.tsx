52898971a6378579b45da213fb0eabe096971097
LilyMart7 <lilymart7@gmail.com>
2020-12-29 14:15:38 +0200

    updated Forgot password page

--- src/ForgotPassword/index.tsx
+++ src/ForgotPassword/index.tsx
@@ -30,6 +30,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     textfield: {
       color: primaryColor,
+      fontSize: 12,
     },
     boxBtn: {
       float: "left",
@@ -37,6 +38,7 @@ const useStyles = makeStyles((theme: Theme) =>
       borderColor: primaryColor,
       color: primaryColor,
       width: "100%",
+      fontSize: 12,
     },
     boxWrapper: {
       margin: 10,
