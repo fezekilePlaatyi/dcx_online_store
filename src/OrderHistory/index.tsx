f56deffe4033913bb1151e49a94db6110e07c127
LilyMart7 <lilymart7@gmail.com>
2021-01-04 15:24:52 +0200

    added order history page styles

--- src/OrderHistory/index.tsx
+++ src/OrderHistory/index.tsx
@@ -22,6 +22,13 @@ import {
 } from "../themes/theme-config";
 import { useHistory, withRouter } from "react-router-dom";
 import InvoiceService from "../services/invoice-service";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Card from "@material-ui/core/Card";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -100,6 +107,85 @@ const useStyles = makeStyles((theme) => ({
   h3: {
     marginBottom: 30,
   },
+  tableRowDescription: {
+    width: "35%",
+    textAlign: "justify",
+    fontSize: 12,
+  },
+  tableCellsQty: {
+    width: "10%",
+    fontSize: 14,
+  },
+  tableRowValue: {
+    fontSize: 12,
+  },
+  tableRowValueName: {
+    fontSize: 12,
+    width: "30%",
+  },
+  tableRowValuePrice: {
+    fontSize: 12,
+    width: "20%",
+  },
+  tableRowValueQty: {
+    fontSize: 12,
+    width: "20%",
+  },
+  tableRowValueDesc: {
+    fontSize: 12,
+    width: "60%",
+    textAlign: "justify",
+  },
+  tableCellsName: {
+    fontSize: 12,
+    width: "%",
+    textAlign: "justify",
+  },
+  tableCellsDate: {
+    fontSize: 12,
+    width: "%",
+    textAlign: "justify",
+    borderBottom: "none !important"
+  },
+  tableCells: {
+    fontSize: 14,
+  },
+  paperContetnt: {
+    display: "flex",
+    flexDirection: "column",
+    //justifyContent: "space-between",
+  },
+  tableRow: {
+    "&:hover": {
+      backgroundColor: "#808080 !important",
+    },
+  },
+  tableCell: {
+    color: backgroundMain,
+  },
+  tableDiv: {
+    width: "100%",
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  tableDivHeading: {
+    width: "100%",
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  rootCard: {
+    padding: 20,
+    overflow: "auto",
+    backgroundColor: backgroundMain,
+    border: "none",
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+  },
+  tableHeading: {
+    // marginBottom: 20,
+    marginTop: 40,
+  },
   productListCardsContainer: {},
 }));
 
@@ -117,19 +203,25 @@ const OrderHistory = () => {
       .then((data) => {
         invoicesData.splice(0, invoicesData.length);
         data.forEach((doc: any) => {
-          invoicesData.push(doc.data().invoiceData)
+          invoicesData.push(doc.data().invoiceData);
         });
 
         invoices.push(
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Price</th>
-              <th>Quantity</th>
-            </tr>
-          </thead>
-        )
+          <TableContainer component={Paper} className={classes.tableDiv}>
+            <Table aria-label="simple table">
+              <TableBody></TableBody>
+            </Table>
+          </TableContainer>
+
+          // <thead>
+          //   <tr>
+          //     <th>Name</th>
+          //     <th>Description</th>
+          //     <th>Price</th>
+          //     <th>Quantity</th>
+          //   </tr>
+          // </thead>
+        );
         setOrderHistory(invoicesData);
       })
       .catch((error) => {
@@ -141,35 +233,72 @@ const OrderHistory = () => {
 
   function addInvoiceToAccordingList(element: any, index: any, array: any) {
     let lineItems: any = [];
+    let lineItemsHTMLBody: any = [];
+    let lineItemHTMLTable: any = [];
+
     let itemLength = element.length;
     let counter = 0;
+    const now = Date.now();
+    let date = new Date(now).toDateString()
 
     element.forEach((item: any) => {
-      counter++
+      counter++;
       lineItems.push(
-        <tr style={{ color: "white", fontSize: "102%" }}>
-          <td>{item.name}</td>
-          <td>{item.description}</td>
-          <td>{item.price}</td>
-          <td>{item.quantity}<br></br></td>
-        </tr >
-      )
-      if (counter == itemLength) {
-        lineItems.push(
-          <tr>
-            <td>111111111111111111111111111111111111111111111111</td>
-            <td>111111111111111111111111111111111111111111111111</td>
-            <td>111111111111111111111111111111111111111111111111</td>
-            <td>111111111111111111111111111111111111111111111111</td>
-          </tr>
-        )
-      }
-    })
+        <TableRow hover className={classes.tableRow} key={element.id}>
+          <TableCell className={classes.tableRowValueName}>
+            {item.name}
+          </TableCell>
+          <TableCell className={classes.tableRowValueDesc}>
+            {item.description}
+          </TableCell>
+          <TableCell className={classes.tableRowValuePrice}>
+            {item.price}
+          </TableCell>
+          <TableCell className={classes.tableRowValueQty}>
+            {item.quantity}
+          </TableCell>
+        </TableRow>
+      );
+    });
 
-    invoices.push(lineItems)
+    lineItemsHTMLBody.push(
+      <TableBody component={Paper} className={classes.tableDiv}>
+        {lineItems}
+      </TableBody>
+    )
 
+    lineItemHTMLTable.push(
+      <div className={classes.tableHeading}>
+        <TableContainer component={Paper} className={classes.tableDivHeading}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell className={classes.tableCellsDate} align="left">
+                  <h4>Date: {date}</h4>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className={classes.tableCellsName} align="left">
+                  Name
+                </TableCell>
+                <TableCell className={classes.tableCells} align="left">
+                  Description
+                </TableCell>
+                <TableCell className={classes.tableCells} align="left">
+                  Price (R)
+                </TableCell>
+                <TableCell className={classes.tableCellsQty} align="left">
+                  Quantity
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            {lineItemsHTMLBody}
+          </Table>
+        </TableContainer>
+      </div>
+    )
 
-
+    invoices.push(lineItemHTMLTable);
 
     // for (const [key, value] of Object.entries(element)) {
     //   var invoice: any = value;
@@ -215,3 +344,5 @@ const OrderHistory = () => {
   );
 };
 export default OrderHistory;
+
+
